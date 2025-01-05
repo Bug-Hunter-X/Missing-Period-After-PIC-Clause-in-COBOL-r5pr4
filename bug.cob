13564183171051e7f9c9523785068bd157080ba5
BugHunter-X <askzenai@gmail.com>
2025-01-05 10:30:05 +0100

    Add bug.cob file

---
+++ bug.cob
@@ -0,0 +1,4 @@
+01  WS-AREA-1.         PIC X(100). 
+01  WS-AREA-2.         PIC X(100). 
+* In the above declaration, if the programmer forgets to add a period (.) after PIC X(100), 
+* it will result in a syntax error or unexpected behavior during compilation.
